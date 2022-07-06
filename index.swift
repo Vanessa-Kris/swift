a8b00431a4c62d8e6ca80dfe86fe587771d0f1b9
Minion <nessakristo@gmail.com>
2022-07-06 13:51:10 +0100

    random shit

---
+++ index.swift
@@ -0,0 +1,11 @@
+var planet = "Earth" 
+
+//
+switch planet {
+  case "Earth","Mecury","Venus","Mars":
+  print("Terrestrial planet")
+  case "Saturn", "Jupitar", "Uranus", "Neptune":
+  print("Jovian planet")
+  default:
+  print("Unknown planet")
+}
